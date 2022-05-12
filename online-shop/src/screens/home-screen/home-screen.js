795670005fccb28d5dae137bec62209ee6b9f08d
Volodymyr Bogryashov <bogryashov01@gmail.com>
2022-05-12 13:03:20 +0300

    adding carousel into project

--- online-shop/src/screens/home-screen/home-screen.js
+++ online-shop/src/screens/home-screen/home-screen.js
@@ -1,8 +1,30 @@
+// Modules
+import Carousel from 'react-bootstrap/Carousel';
+import styled from 'styled-components';
+
+// Styles
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './home-screen.css';
+
 function HomeScreen() {
   return (
-    <div>
-      <div>olol</div>
-    </div>
+    <CarouselContainer>
+      <Carousel>
+        <Carousel.Item>
+          <div>olo</div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div>olo</div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div>olo</div>
+        </Carousel.Item>
+      </Carousel>
+    </CarouselContainer>
   );
 }
+
+const CarouselContainer = styled.div`
+  height: 100vh;
+`;
 export default HomeScreen;
