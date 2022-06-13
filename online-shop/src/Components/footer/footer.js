48daacc4efdf5b63ee022cfd8ad5f42a2d22086b
Volodymyr Bogryashov <bogryashov01@gmail.com>
2022-06-13 14:20:04 +0300

    footer skeleton

--- online-shop/src/Components/footer/footer.js
+++ online-shop/src/Components/footer/footer.js
@@ -1,3 +1,33 @@
-function Footer() {}
+// Modules
+import styled from 'styled-components';
+
+function Footer() {
+  return (
+    <FooterBlock>
+      <Wrapper>
+        <FooterItem>logo</FooterItem>
+        <FooterItem>Links</FooterItem>
+        <FooterItem>Sponsors</FooterItem>
+      </Wrapper>
+    </FooterBlock>
+  );
+}
+
+const FooterBlock = styled.div`
+  background-color: #ffb200;
+  height: 400px;
+  margin-top: 100px;
+`;
+const Wrapper = styled.div`
+  max-width: 1400px;
+  margin: auto;
+  padding-top: 125px;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+`;
+
+const FooterItem = styled.div`
+  justify-self: center;
+`;
 
 export default Footer;
